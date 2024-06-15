fcdb1a5ff9fff31ae0080038d15946b63d1cb754
Matthew Bolofinde <molanrewajuking@gmail.com>
2024-06-15 11:33:41 -0500

    added pythontest file

---
+++ pythontest.py
@@ -0,0 +1,14 @@
+def sum(nums):
+    """
+    array = [1,2,3,4]
+    sum = 10
+
+
+    """
+
+    sum = 0
+
+    for num in nums:
+        sum += num
+    return sum 
+
---
+++ weatherfile.py
@@ -0,0 +1,6 @@
+import json 
+import pprint
+
+with open("../../samplefile.data", 'r') as weather:
+    weatherdata = json.load(weather)
+
